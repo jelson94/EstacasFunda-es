9b638871b9faba1de3e72364192969547c8894f8
jelson94 <jelson.eng.civil@gmail.com>
2024-03-01 16:34:38 -0300

    Adicionei uma função para calcular o volume da estaca

--- funcEstacas.py
+++ funcEstacas.py
@@ -6,3 +6,9 @@ def areaEstaca(diametro):
     area = pi * (diametro ** 2) / 4
 
     return area
+
+def volumeEstaca(diametro, prof):
+
+    volumeEst = areaEstaca(diametro) * prof
+
+    return volumeEst
